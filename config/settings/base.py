d9c4163f4ce7d9f4f7b4db85a85f0f0a94ecd90b
Jeremy Torella <torella.jeremy@gmail.com>
2018-04-19 15:19:29 -0400

    Update base.html to include expected dependencies
    Update base.html file to include all dependencies needed for
    the project. Also fix base.py to properly include coinhub database

--- config/settings/base.py
+++ config/settings/base.py
@@ -56,6 +56,7 @@ LOCAL_APPS = [
     # custom users app
     'coinhub.users.apps.UsersConfig',
     # Your stuff: custom apps go here
+    'coinhub'
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
@@ -99,7 +100,7 @@ EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.s
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = [
-    ("""Andy Wu""", 'andywu6079@gmail.com'),
+    ("""Andy Wu, Jeremy Torella""", 'andywu6079@gmail.com'),
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#managers
@@ -111,7 +112,13 @@ MANAGERS = ADMINS
 # Uses django-environ to accept uri format
 # See: https://django-environ.readthedocs.io/en/latest/#supported-types
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres:///coinhub'),
+    'default': {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "coinhub",
+        "HOST": "localhost",
+        "PORT": "5432",
+        "PASSWORD": "test1234",
+    }
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
